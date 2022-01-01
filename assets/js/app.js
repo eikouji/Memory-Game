b5bf6f3769904d6285931607420e066d552dedaa
Eiko Ujifusa <eikorouji@gmail.com>
2021-12-31 20:22:32 -0800

    Commented in my ideas, app.js, line 131
    Put down some ideas on how to make my game something people can play to win. So far, images are generated upon the game starting, but pairs do not generate. I just need a way to make 6 random images appear, and double them to make a working grid of 12 images with 6 pairs of cards.

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,47 +79,86 @@ document.addEventListener('DOMContentLoaded', () => {
       const optionOneId = cardsChosenId[0]
       const optionTwoId = cardsChosenId[1]
 
- // a match is found // 
       if(optionOneId == optionTwoId) {
           cards[optionOneId].setAttribute('src', './assets/images/empty-board-tile.png')
           cards[optionTwoId].setAttribute('src', './assets/images/empty-board-tile.png')
+          alert('You have clicked the same image!')
+
       }
+
       else if (cardsChosen[0] === cardsChosen[1]) {
           alert('You found a matching pair!')
           cards[optionOneId].setAttribute('src', './assets/images/empty-board-tile.png')
           cards[optionTwoId].setAttribute('src', './assets/images/empty-board-tile.png')
           
-          cardsWon.push(cardsChosen)
           cards[optionOneId].removeEventListener('click', flipCard)
           cards[optionTwoId].removeEventListener('click', flipCard)
+          cardsWon.push(cardsChosen)
 
-// if cards do not match, flip card back over to show picnic blanket img card backing //
+        // if cards do not match, flip card back over to show picnic blanket img card backing //
       } else {
           cards[optionOneId].setAttribute('src', './assets/images/picnic-blanket.png')
           cards[optionTwoId].setAttribute('src', './assets/images/picnic-blanket.png')
           alert('No match, try again!')
       }
+
         cardsChosen = []
         cardsChosenId = []
 
-// displays the game score //
+        // displays the game score //
         resultDisplay.textContent = cardsWon.length
           if (cardsWon.length === cardArray.length/2) {
               resultDisplay.textContent = 'Congratulations! You found ALL the matches! Enjoy your picnic!'
           }
       }
 
-  // flip your card // 
-  function flipCard() {
+        // flip your card // 
+    function flipCard() {
       let cardId = this.getAttribute('data-id')
         cardsChosen.push(cardArray[cardId].name)
         cardsChosenId.push(cardId)
       this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 600)
+        if (cardsChosen.length ===2) {
+            setTimeout(checkForMatch, 500)
         }
-  }
+    }
 
   createBoard()
 
 })
+
+
+/* --------------------
+Issue notes and ideas:
+
+Pairs do not generate at start of game. 
+
+There are 12 cards, and 12 items for each card. It is a 1 - 1 ratio, which is fine, but does
+not generate repeating cards. 
+
+I need to repeat a total of 6 pairs to make a full 
+grid of 12 cards, and therefore a full game, that can actually play 
+and keep score and clear the board.
+
+
+Either I can just use 6 items and make them show up twice by adding the same image twice,
+which would make me repeat code, and all that. But, that would generate one puzzle, and be kind of
+boring, and not really interesting, as the same images would show up.
+
+Or,
+
+I make it so that from the array of 12, or however many images I want the array to have,
+the grid shows a random assortment of 6 fruit images
+and then doubles the chosen image to make a pair. That way, there's variety every time
+a new game is generated, but there will be pairs based on the randomly chosen 6 cards.
+
+And, I can add as many fruit or food pictures to the same array because no matter how large
+the array, a random bunch of 6 will be used, and doubles will be generated to create working pairs.
+
+I can keep all the fruits I've used up until this point, and retain my original 3 x 4 grid, or however
+big I want it to be.
+
+How to do this? 
+
+
+----------- */
