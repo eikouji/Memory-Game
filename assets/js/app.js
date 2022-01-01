1b0f0929c49aee6664974632216dd013e1279037
Eiko Ujifusa <eikorouji@gmail.com>
2022-01-01 09:57:03 -0800

    Fixed a few image names, and fixed Sandwich icon
    sandwich icon fixed. changed soda-pop to soda-pop-drink and adjusted the image name accordingly.

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,8 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
         img: './assets/images/coffee-icon.png'
       },
       {
-        name: 'soda-pop',
-        img: './assets/images/cup-with-straw-icon.png'
+        name: 'soda-pop-drink',
+        img: './assets/images/drink-with-straw-icon.png'
       },
       {
         name: 'kiwi-fruit',
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
       },
       {
         name: 'sandwich',
-        img: './assets/images/sandwich.png'
+        img: './assets/images/sandwich-icon.png'
       }
     ]
   
@@ -145,59 +145,28 @@ grid of 12 cards, and therefore a full game, that can actually play
 and keep score and clear the board.
 
 
-Either I can just use 6 items and make them show up twice by adding the same image twice,
-which would make me repeat code, and all that. But, that would generate one puzzle, and be kind of
-boring, and not really interesting, as the same images would show up.
-
-Or,
-
-I make it so that from the array of 12, or however many images I want the array to have,
-the grid shows a random assortment of 6 fruit images
-and then doubles the chosen image to make a pair. That way, there's variety every time
-a new game is generated, but there will be pairs based on the randomly chosen 6 cards.
-
-And, I can add as many fruit or food pictures to the same array because no matter how large
-the array, a random bunch of 6 will be used, and doubles will be generated to create working pairs.
-
-I can keep all the fruits I've used up until this point, and retain my original 3 x 4 grid, or however
-big I want it to be.
-
-How to do this? 
-
-Randomize Cards after clearing the board
-
- if (cardsWon.length===cardArray.length/2) {
-        resultDisplay.textContent ='Congratulations! You found them all.'
-        redrawBoard()
-        cardsWon = [ ]
-        setTimeout(()=>{ resultDisplay.textContent = '' },1500)
-    }
-
-// Function redrawBoard()
+One solution -- the one used in the tutorial:
+1. Make 6 total cards with unique images
+2. Add them twice to make 12 images
+3. The array will sample from the array, and pairs will be generated guaranteed.
 
 
-function redrawBoard() {
-    var cards = document.querySelectorAll('img')
-    for(let i=0; i < cards.length; i++) {
-      cards[i].setAttribute('src','images/blank.png')
-    }
-    randomizeCards()
-  }
+My solution
+1. Make an array of 12 - 16 images.
+2. Take from that array 6 random images.
+3. Duplicate the images of the randomly chosen 6 images to populate a grid of 12 cards.
+4. Clear the board when pairs are found. 
+5. Upon the generation of a new game, a different set of randomly chosen cards (6) will populate
+the game board, creating variety.
+6. keep playing and clearing the board, with new boards generating after each clearing.
 
-
-// Function randomizeCards
-
-
-  function randomizeCards() {
-    cardArray.sort(()=> 0.5 - Math.random())
-  }
+7. Don't break the other functions!
 
 
 
 
 
+----------- */
+createBoard()
 
-
-
-
------------ */});
+})
